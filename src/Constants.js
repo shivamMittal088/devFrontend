aed7f6d94f8c6ae645452093c38ebae742ce0221
shivamMittal088 <shivamofficial.sm@gmail.com>
2025-11-24 22:55:40 +0530

    b

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
 export const devLogo = "https://play-lh.googleusercontent.com/EnxJKkoXzxrmA0RA0gUie0K4gvbRLrQhpflyMzwSMvCfk2FA6o9sBLzfWtG5qLzIBIc=w240-h480-rw"
 
-export const URL = "/api/";
+export const URL = "http://localhost:5555/";
