9824237c9a1d975afa47bb98fd1c233a6e287abd
shivam mittal <shivamofficialsm@gmail.com>
2026-01-05 06:15:31 +0530

    Update URL constant for API usage

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 export const devLogo = "https://play-lh.googleusercontent.com/EnxJKkoXzxrmA0RA0gUie0K4gvbRLrQhpflyMzwSMvCfk2FA6o9sBLzfWtG5qLzIBIc=w240-h480-rw"
 
 
-export const URL = "http://localhost:5555/"
-// export const URL = "/api/";
+// export const URL = "http://localhost:5555/"
+export const URL = "/api/";
